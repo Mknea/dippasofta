3e70f99afdd8ce556e1c5f62293d86779f84d065
Aleksi Mäkinen <aleksi.makinen@etteplan.com>
2019-11-29 16:57:18 +0200

    Added printing to tables

--- cotpquery.py
+++ cotpquery.py
@@ -6,7 +6,8 @@ Source: https://github.com/tijldeneut/ICSSecurityScripts/blob/master/FullSiemens
 import sys, socket, re, string
 import argparse
 from binascii import hexlify, unhexlify
-from robot.api.deco import keyword
+from robot.api.deco import keyword          # Modifying Robot Framework keyword call
+from texttable import Texttable             # Printing result to table
 
 def getInfoViaCOTP(targetIP):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,11 +21,9 @@ def getInfoViaCOTP(targetIP):
     data = '720100b131000004ca0000000200000120360000011d00040000000000a1000000d3821f0000a3816900151653657276657253657373696f6e5f3742363743433341a3822100150b313a3a3a362e303a3a3a12a3822800150d4f4d532b204465627567676572a38229001500a3822a001500a3822b00048480808000a3822c001211e1a304a3822d001500a1000000d3817f0000a38169001515537562736372697074696f6e436f6e7461696e6572a2a20000000072010000'
     tpktlength = str(hex((len(data)+14)/2))[2:] ## Dynamically find out the data length
     cotpdata = send_and_recv(sock, '030000'+tpktlength+'02f080'+data)
-
+    #print(cotpdata)
     ## It is sure that the CPU state is NOT in this response
-    print('Hardware: '+cotpdata.split(';')[2])
-    print('Firmware: '+filter(lambda x: x in string.printable, cotpdata.split(';')[3].replace('@','.')))
-
+    parse_and_log_results(cotpdata)
     sock.close()
 
 def send_and_recv(sock, strdata, sendOnly = False):
@@ -33,6 +32,16 @@ def send_and_recv(sock, strdata, sendOnly = False):
     if sendOnly: return
     ret = sock.recv(65000)
     return ret
+
+def parse_and_log_results(data):
+    t = Texttable()
+    t.add_row(['Hardware ID', 'Firmware version'])
+    hardware = data.split(';')[2]
+    firmware = filter(lambda x: x in string.printable, data.split(';')[3].replace('@','.'))
+    t.add_row([hardware, firmware])
+    t.set_max_width(0)
+    t.set_cols_dtype(["t", "t"])
+    print(t.draw())
 
 def isIpv4(ip):
     match = re.match("^(\d{0,3})\.(\d{0,3})\.(\d{0,3})\.(\d{0,3})$", ip)
